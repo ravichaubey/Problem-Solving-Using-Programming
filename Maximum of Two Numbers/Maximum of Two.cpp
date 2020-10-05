71195a36d3bb0a919c41b1b50918b4a368090b25
Ravi Chaubey <ravichaubey43@gmail.com>
2020-10-05 10:26:06 +0530

    New Program

---
+++ Maximum of Two Numbers/Maximum of Two.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Please enter two numbers ==> ";
+    cin>>a>>b;
+    if(a>b){
+        cout<<"First Number is Greater";
+    }else
+    {
+        cout<<"Second Number is Greater";
+    }
+    return 0;
+    
+}
---
+++ Maximum of Two Numbers/Maximum of Two.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 10:22:45 2020
+
+@author: ravi
+"""
+a,b = map(float,input("Please enter two number ==> ").split())
+
+if a>b:
+    print("First Number is Greater")
+else:
+    print("Second Number is Greater")
